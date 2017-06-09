b20e993752ef8e1ffe45592bdd34950bc6061763
Amanda Johnson <amandajnej@gmail.com>
2017-06-09 17:22:02 +0100

    Add initial HTML, CSS and script file.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#survey").submit(function(event) {
+    var name = $("#name").val();
+    var age1 = $("#age1").val();
+    var age2 = $("#age2").val();
+    var age3 = $("#age3").val();
+    var age4 = $("#age4").val();
+
+  alert("thank you for filling out the survey!");
+  
+
+  event.preventDefault();
+  })
+
+
+
+})
